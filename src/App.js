e3630a5661673eafd0de16658ba41eab6a0153a8
Seun Soetan <adewemimo03@gmail.com>
2021-01-15 11:19:32 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import UserList from "./Components/studentList";
+import React from 'react';
+
+class App extends React.Component {
+  render() { 
+    return ( 
+      <>
+      <UserList />
+      </>
+     
+     );
+  }
+}
+ 
+export default App;
+ 
